b0e39a54bb6371a59ec0a07028b123ca66188571
Bhupesh Kumar <bhupeshkumar003@gmail.com>
2020-03-05 21:07:53 +0530

    Updates

---
+++ precache-manifest.e8fc82a468491e46d1f2f664a1bf60d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baebd4a1364c641cdb1056cbc5d75ac6",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "927916d3523b4d4fb7f0",
+    "url": "/facerecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d49a566d8a3b15bd134c",
+    "url": "/facerecognition/static/css/main.e83f5554.chunk.css"
+  },
+  {
+    "revision": "927916d3523b4d4fb7f0",
+    "url": "/facerecognition/static/js/2.0e855ce7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/facerecognition/static/js/2.0e855ce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d49a566d8a3b15bd134c",
+    "url": "/facerecognition/static/js/main.12f4fc14.chunk.js"
+  },
+  {
+    "revision": "f07f4d18e51ab824cadd",
+    "url": "/facerecognition/static/js/runtime-main.11e2254c.js"
+  }
+]);
